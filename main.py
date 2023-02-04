088c873436bdb10256eb875f72630aff3d1b9655
nickrettino <88734682+nickrettino@users.noreply.github.com>
2023-02-04 11:01:42 -0500

    fixed background

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+"""server side script"""
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -5,6 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def login():
+    """login page"""
     return render_template("index.html")
 
 
