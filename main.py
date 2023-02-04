6976cdc5cb240ec8af85bee217204c2f63e981b5
nickrettino <88734682+nickrettino@users.noreply.github.com>
2023-02-04 11:17:21 -0500

    added input page
    page is up and running properly

--- main.py
+++ main.py
@@ -10,5 +10,10 @@ def login():
     return render_template("index.html")
 
 
+@app.route("/input")
+def input_page():
+    return render_template("main_page.html")
+
+
 if __name__ == "__main__":
     app.run(debug=True)
